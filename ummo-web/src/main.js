f92a3bff7121e3c444b273659189c516dcef5903
mosaic141688 <mbhamalimosaic@gmail.com>
2018-09-29 16:41:03 +0200

    Added server url

--- ummo-web/src/main.js
+++ ummo-web/src/main.js
@@ -5,6 +5,11 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
+const server_url = process.env.NODE_ENV === 'production' ?
+    `${window.location}`
+    : 'http://localhost:3000'
+window.server_url = `${server_url}/api`
+
 Vue.config.productionTip = false
 
 new Vue({
